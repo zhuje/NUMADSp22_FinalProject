eaf36398903178e1c70c16df222de4c9a6793138
zhuje <59589720+zhuje@users.noreply.github.com>
2022-03-31 15:44:13 -0400

    Revised Login, Built Register, Built Profile, Updated Android Theme to Green instead of Purple

--- app/src/main/java/edu/neu/madcourse/numadsp22_finalproject/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numadsp22_finalproject/MainActivity.java
@@ -2,9 +2,14 @@ package edu.neu.madcourse.numadsp22_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
     }
+
+
+    public void onClick(View view){
+        Intent intent = new Intent(this, LoginRegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
