2f08128ef75cf1028a17c4d9d26f4a9027541438
Hannah <vining.h@husky.neu.edu>
2022-04-06 23:31:48 -0400

    Finished coding part to allow for playing audio files.

--- app/src/main/java/edu/neu/madcourse/numadsp22_finalproject/VerbViewAdaptor.java
+++ app/src/main/java/edu/neu/madcourse/numadsp22_finalproject/VerbViewAdaptor.java
@@ -1,14 +1,18 @@
 package edu.neu.madcourse.numadsp22_finalproject;
 
 import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.MediaPlayer;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 // This class helps show the Verb's CardView within its list.
@@ -59,6 +63,49 @@ public class VerbViewAdaptor extends BaseAdapter {
         // set picture
         character.setImageResource(verbList.get(i).getCharacterPicture());
         // TO DO! SETTING BUTTON TO CORRECT CODE FOR AUDIO
+        Button audioButton = view.findViewById(R.id.audioButton);
+        // set it to play audio
+        audioButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // make a thread to start the audio
+                Runnable audioRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("Tag", "thread part 1");
+                        try {
+                            String audioURL = verbList.get(i).getAudioURL();
+                            Log.d("Tag", "thread part 2");
+                            // using android studio developers documentation on mediaPlayer
+                            // for the below code snippet regarding streaming via HTTP
+                            Log.d("Tag", "thread part 3");
+                            MediaPlayer audioPlayer = new MediaPlayer();
+                            Log.d("Tag", "thread part 4");
+                            audioPlayer.setAudioAttributes(
+                                new AudioAttributes.Builder()
+                                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                .setUsage(AudioAttributes.USAGE_MEDIA)
+                                .build()
+                            );
+                            Log.d("Tag", "thread part 5");
+                            audioPlayer.setDataSource(audioURL);
+                            Log.d("Tag", "thread part 6");
+                            audioPlayer.prepare();
+                            Log.d("Tag", "thread part 7");
+                            audioPlayer.start();
+                            Log.d("Tag", "thread part 8");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+                // now make thread and run it!
+                Thread audioThread = new Thread(audioRunnable);
+                // run it
+                audioThread.start();
+            }
+        });
         return view;
     }
 }
