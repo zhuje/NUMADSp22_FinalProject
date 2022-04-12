8405c7b66264ea141ce31290759a6258d5ab0afa
Hannah <vining.h@husky.neu.edu>
2022-04-12 02:20:59 -0400

    Added code for firestore retrieval.

--- app/src/main/java/edu/neu/madcourse/numadsp22_finalproject/MainLessonsScreen.java
+++ app/src/main/java/edu/neu/madcourse/numadsp22_finalproject/MainLessonsScreen.java
@@ -1,16 +1,32 @@
 package edu.neu.madcourse.numadsp22_finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainLessonsScreen extends AppCompatActivity {
+
+    private FirebaseUser authUserProfile;
+    private int userRank;
+    private String userUUID;
+    private DatabaseReference databaseReference;
 
     final int LOCK = R.drawable.redlock2;
     final int UNLOCK = R.drawable.greenlock2;
@@ -21,15 +37,37 @@ public class MainLessonsScreen extends AppCompatActivity {
     ListView listView;
 
     // TESTING-PURPOSES ONLY, placeholder for rank
-    int userRank = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lessons_screen);
-
         // TODO -- get user rank from db
+        // set as nominal value for error checking
+        userRank = -1;
+        authUserProfile = FirebaseAuth.getInstance().getCurrentUser();
+        userUUID = authUserProfile.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         // pseduocode | user.child("Users").child(authUserProfile.getUID()) -> data snapshot -> rank
+        DatabaseReference userRankRef = databaseReference.child("Users").child(userUUID).child("rank");
+        // add listener to check for changes in values?
+        userRankRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Integer value = dataSnapshot.getValue(Integer.class);
+                if (value != null){
+                    userRank = value;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Error",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        });
 
         if (userRank >= 5) {
             lockList[1] = UNLOCK;
